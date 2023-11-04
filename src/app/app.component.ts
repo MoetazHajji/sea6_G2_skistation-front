6655bbc74cdd2330651f29bd0dab8fa1684a39d6
MoetazHajji <moetaz.hajji@esprit.tn>
2023-11-05 08:17:42 +1300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'sea6_G2_skistation-front';
+}
